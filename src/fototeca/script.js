4e9ce0ad8fec60cb3087418e859f206ec80ad7e8
clovisbd <127855721+clovisbd@users.noreply.github.com>
2026-04-29 09:03:03 -0300

    Update script.js

--- src/fototeca/script.js
+++ src/fototeca/script.js
@@ -1,107 +1,56 @@
 let ocorrencias = [];
 let indiceAtual = 0;
+let typeName = null;
+
+const loader = document.createElement("div");
+loader.innerText = "Carregando dados...";
+loader.style.position = "fixed";
+loader.style.top = "50%";
+loader.style.left = "50%";
+loader.style.transform = "translate(-50%, -50%)";
+loader.style.padding = "12px 20px";
+loader.style.background = "#000";
+loader.style.color = "#fff";
+loader.style.display = "none";
+loader.style.zIndex = 9999;
+document.body.appendChild(loader);
+
+function showLoader(show) {
+  loader.style.display = show ? "block" : "none";
+}
 
 require([
- "esri/Map",
- "esri/views/MapView",
- "esri/layers/GraphicsLayer",
- "esri/Graphic"
-], function (Map, MapView, GraphicsLayer, Graphic) {
- // Endpoint WFS público
- const wfsUrl = "https://observatorio.infraestrutura.mg.gov.br/server/services/Hosted/ICM_PONTO_MAIO_2025_PILOTO_SREMG/MapServer/WFSServer";
- const typeName = "Hosted_ICM_PONTO_MAIO_2025_PILOTO_SREMG";
- const graphicsLayer = new GraphicsLayer();
- const map = new Map({
- basemap: "streets",
- layers: [graphicsLayer]
- });
- const view = new MapView({
- container: "map",
- map: map,
- center: [-44, -19],
- zoom: 7
- });
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/GraphicsLayer",
+  "esri/layers/FeatureReductionCluster",
+  "esri/Graphic"
+], function (Map, MapView, GraphicsLayer, FeatureReductionCluster, Graphic) {
 
- function montarFiltroWFS({ ano, mes, rod, tipo }) {
- return `ano_avalia = ${ano} AND mes_avalia = ${mes} AND ROD = '${rod}' AND tipo = '${tipo}'`;
- }
- function carregarDados(filtroCQL) {
- const params = new URLSearchParams({
- service: "WFS",
- version: "2.0.0",
- request: "GetFeature",
- typeNames: typeName,
- outputFormat: "application/json",
- srsName: "EPSG:4326",
- cql_filter: filtroCQL
- });
- fetch(`${wfsUrl}?${params.toString()}`)
- .then(res => res.json())
- .then(geojson => {
- ocorrencias = geojson.features.sort(
- (a, b) => a.properties.KM - b.properties.KM
- );
- graphicsLayer.removeAll();
+  const wfsUrl =
+    "https://observatorio.infraestrutura.mg.gov.br/server/services/Hosted/ICM_PONTO_MAIO_2025_PILOTO_SREMG/MapServer/WFSServer";
 
+  /* ============================================================
+     1) Descobre o typeName automaticamente (GetCapabilities)
+     ============================================================ */
+  async function validarTypeName() {
+    const resp = await fetch(`${wfsUrl}?service=WFS&request=GetCapabilities`);
+    const xmlTxt = await resp.text();
+    const xml = new DOMParser().parseFromString(xmlTxt, "text/xml");
+    typeName = xml
+      .getElementsByTagName("FeatureType")[0]
+      .getElementsByTagName("Name")[0]
+      .textContent;
 
- ocorrencias.forEach(f => {
- const g = new Graphic({
- geometry: {
- type: "point",
- longitude: f.geometry.coordinates[0],
- latitude: f.geometry.coordinates[1]
- },
- attributes: f.properties,
- symbol: {
- type: "simple-marker",
- color: "red",
- size: 8
- }
- });
- graphicsLayer.add(g);
- });
-
-
- indiceAtual = 0;
- mostrarOcorrencia();
- view.goTo(graphicsLayer.graphics);
- });
+    console.log("typeName detectado:", typeName);
   }
 
- function mostrarOcorrencia() {
-    if (!ocorrencias.length) return;
-
-
- const o = ocorrencias[indiceAtual].properties;
- const [lon, lat] = ocorrencias[indiceAtual].geometry.coordinates;
-
-
- document.getElementById("foto").src = o.Imagem;
- document.getElementById("info").innerHTML = `
- <strong>Rodovia:</strong> ${o.ROD}<br>
- <strong>Km:</strong> ${o.KM}<br>
- <strong>Tipo:</strong> ${o.tipo}<br>
- <strong>Mês/Ano:</strong> ${o.mes_avalia}/${o.ano_avalia}
-    `;
- view.goTo({ center: [lon, lat], zoom: 16 });
-  }
-
- document.getElementById("btnFiltrar").onclick = () => {
- const filtro = montarFiltroWFS({
- ano: ano.value,
- mes: mes.value,
- rod: rodovia.value,
- tipo: tipo.value
- });
- carregarDados(filtro);
- };
- prev.onclick = () => {
- indiceAtual = (indiceAtual - 1 + ocorrencias.length) % ocorrencias.length;
- mostrarOcorrencia();
- };
- next.onclick = () => {
- indiceAtual = (indiceAtual + 1) % ocorrencias.length;
- mostrarOcorrencia();
-  };
-
-});
+  /* ============================================================
+     2) Camada com clusterização
+     ============================================================ */
+  const graphicsLayer = new GraphicsLayer({
+    featureReduction: new FeatureReductionCluster({
+      clusterRadius: "100px",
+      labelingInfo: [{
+        labelExpressionInfo: { expression: "$feature.cluster_count" },
+        symbol: {
